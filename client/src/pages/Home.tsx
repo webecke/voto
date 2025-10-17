71fdf9344845611e921bc49b4ffaf8f7e2531cfa
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2025-10-17 00:17:02 -0600

    make coming soon

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,9 +3,11 @@ import styles from './Home.module.css';
 function Home() {
   return (
     <div className={styles.container}>
-      <img src="/voto.svg" alt="Voto" className={styles.logo} />
-      <h1>voto</h1>
-      <p>Group decision making made quick and easy</p>
+      <div className={styles.logoSection}>
+        <img src="/voto.svg" alt="Voto" className={styles.logo} />
+        <p className={styles.tagline}>Group decisions, made easy</p>
+      </div>
+      <p className={styles.comingSoon}>Coming Soon</p>
     </div>
   );
 }
