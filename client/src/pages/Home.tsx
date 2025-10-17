0812f9a90ffc3bf63dc1fc3cdf219c93eb590f0d
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2025-10-16 23:29:52 -0600

    set up routing and cleaner css

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import styles from './Home.module.css';
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>voto</h1>
+      <p>Group decision making made quick and easy</p>
+    </div>
+  );
+}
+
+export default Home;
