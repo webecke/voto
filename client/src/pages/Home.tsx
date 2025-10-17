042cf3ccdf77e4c0c388fe4d25cdf4b8bcf9d76f
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2025-10-17 00:11:38 -0600

    add logo

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import styles from './Home.module.css';
 function Home() {
   return (
     <div className={styles.container}>
+      <img src="/voto.svg" alt="Voto" className={styles.logo} />
       <h1>voto</h1>
       <p>Group decision making made quick and easy</p>
     </div>
